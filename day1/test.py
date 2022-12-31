7cfc2305308426066be40ca90593713d0297c363
jaeminstan <hwasan93@gmail.com>
2022-12-31 11:32:40 +0900

    조건문

---
+++ day1/test.py
@@ -0,0 +1,18 @@
+# print("안녕 파이썬يييييييييييبس")
+# i=0
+# print(i)
+# j=10
+# print(i+j)
+apple=10
+orange=20
+print(orange+apple)
+print(orange-apple)
+print(orange*apple)
+print(orange/apple)
+print(orange%apple)
+print(orange**apple)
+print(3**3)
+print(10/3)
+print(10%3)
+print(10//3)
+print(orange//apple)
