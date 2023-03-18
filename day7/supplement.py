9d5d336c6e114863e29114a2d1f49c9481b4e28d
jaeminstan <hwasan93@gmail.com>
2023-03-18 11:24:18 +0900

    day7supplement.py

---
+++ day7/supplement.py
@@ -0,0 +1,161 @@
+# import matplotlib.pyplot as plt
+# plt.figure()
+# x=[]
+# y=[]
+# for i in range(-10,11):
+#     x.append(i)
+#     y.append(-2*i+1)
+
+# plt.plot(x,y)
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# plt.figure()
+# x=[]
+# y=[]
+
+# for i in range(-20,21):
+#     x.append(i)
+#     y.append(-1*i*i+2*i-3)
+
+
+# plt.plot(x,y)
+# plt.show()
+
+
+
+
+# import matplotlib.pyplot as plt
+# plt.figure()
+# x=[]
+# y=[]
+
+# for i in range(-10,11):
+#     x.append(i)
+#     y.append(2*i*i*i-8)
+
+
+# plt.plot(x,y)
+# plt.show()
+
+
+
+
+
+
+
+# import matplotlib.pyplot as plt
+# plt.figure()
+# x=[]
+# x1=[]
+# y=[]
+# y1=[]
+
+# for i in range(-10,11):
+#     x.append(i)
+#     x1.append(i)
+#     y.append(2**i)
+#     y1.append(1/2**i)
+
+
+# plt.plot(x,y,x1,y1)
+# plt.show()
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# plt.figure()
+# x=[]
+# y=[]
+
+# for i in range(-60,70):
+#     x.append(i)
+#     y.append(np.tan(i/10))
+# plt.plot(x,y)
+# plt.show()
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+x=[]
+x1=[]
+x2=[]
+x3=[]
+y=[]
+y1=[]
+y2=[]
+y3=[]
+
+
+
+for i in range(-10,11):
+    x.append(i)
+    x1.append(i)
+    x2.append(i)
+    x3.append(i)
+    y.append(-i)
+    y1.append(i*i)
+    y2.append(i)
+    y3.append(-i*i)
+plt.plot(x,y,'o',x1,y1,'o',x2,y2,'o',x3,y3,'o')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
